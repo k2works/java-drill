e9299277c0bb1aae7a947a1e738d2ce67d3fd345
k2works <kakimomokkuri@gmail.com>
2021-07-02 18:35:53 +0900

    test: HelloWorld

--- app/src/test/java/drill/AppTest.java
+++ app/src/test/java/drill/AppTest.java
@@ -7,8 +7,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test
+    void 何か便利なもの() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    void 簡単な挨拶を返す() {
+        assertEquals("Hello from Java", Program.helloWorld());
+    }
+
+    @Test
+    void 指定された名前で挨拶を返す() {
+        assertEquals("Hello from VSCode", Program.helloWorld("VSCode"));
+    }
+}
+
+class Program {
+    static String helloWorld(String name) {
+        return String.format("Hello from %s", name);
+    }
+
+    public static Object helloWorld() {
+        return "Hello from Java";
     }
 }
